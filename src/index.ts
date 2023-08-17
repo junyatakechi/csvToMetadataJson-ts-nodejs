a94a363d1679df3333ec2615e94c78854a31a2bc
junyatakechi <takechi48j@gmail.com>
2023-08-17 16:49:18 +0900

    変更: 出力方法

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import * as fs from 'fs';
+import * as fs from 'fs/promises'; 
 import * as readline from 'readline';
 import Papa from 'papaparse';
 import * as path from 'path';
@@ -50,25 +50,23 @@ const rl = readline.createInterface({
 const csvFilePath = process.argv[2];
 
 if (!csvFilePath) {
-    console.error('Please provide the path to the CSV file as a command line argument.');
+    console.error('CSVファイルのパスを引数に設定してください。');
     process.exit(1);
 }
 
-rl.question('Enter the column index where the attributeFields start (0-based): ', (attributeFieldsStartIndex) => {
+rl.question('Attributesを記入しているのは何列目からですか？一番最初の列を0番とします。', async (attributeFieldsStartIndex) => {
     rl.close();
-    fs.readFile(csvFilePath, 'utf8', (err, csvData) => {
-        if (err) {
-            console.error('Failed to read CSV file:', err);
-            return;
-        }
+    try {
+        const csvData = await fs.readFile(csvFilePath, 'utf8');
         const jsonMetadata = csvToJson(csvData, Number(attributeFieldsStartIndex));
-        const outputPath = path.join(path.dirname(csvFilePath), 'output.json');
-        fs.writeFile(outputPath, JSON.stringify(jsonMetadata, null, 2), (writeErr) => {
-            if (writeErr) {
-                console.error('Failed to write JSON file:', writeErr);
-                return;
-            }
-            console.log(`JSON metadata has been written to ${outputPath}`);
-        });
-    });
+        const outputDir = path.join(path.dirname(csvFilePath), path.basename(csvFilePath, '.csv'));
+        await fs.mkdir(outputDir, { recursive: true });
+        await Promise.all(jsonMetadata.map((metadata, index) => {
+            const outputPath = path.join(outputDir, `${index + 1}`);
+            return fs.writeFile(outputPath, JSON.stringify(metadata, null, 2));
+        }));
+        console.log(`出力されました。パス: ${outputDir}`);
+    } catch (err) {
+        console.error('エラー: CSVファイルの処理で問題がありました。', err);
+    }
 });
